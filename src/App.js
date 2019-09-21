c7d7ed1260fbb6969fb26a1cf8369394764bdf69
Ariyapoom <47807511+Ariyapoom@users.noreply.github.com>
2019-09-21 21:23:39 +0700

    Fix Wrong Words

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react'
 import './App.css'
 import WordCard from './WordCard'
-let words = ['monster','hello','system','hydrogen','oxigen']
+let words = ['monster','hello','system','hydrogen','oxygen']
 class App extends Component{
   render(){
     let num = Math.floor(Math.random()*words.length);
